25e0385f19f9010322f3eb3af2524a01ecaea756
Dogukan (Dan) Mere <meredogukan@gmail.com>
2022-12-23 15:41:13 -0500

    header/hero section completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,17 +10,17 @@ const Header = (props: Props) => {
       {/* Navbar */}
       <Navbar />
       {/* Text + Button */}
-      <div className='w-full px-[24px] md:px-[40px] py-[56px] md:py-[80px] text-white w-[98%] md:w-[70%]'>
+      <div className='w-full z-[5] px-[24px] md:px-[40px] py-[56px] md:py-[80px] text-white w-[98%] md:w-[70%] lg:w-full lg:flex lg:flex-col lg:text-center lg:max-w-[780px] lg:m-auto'>
         <h1 className='font-bold font-secondary text-[48.25px] uppercase leading-[95%] tracking-[0.1rem] md:text-[4.4rem]'>
           Imagine a place...
         </h1>
-        <p className='mt-[24px] md:mt-[40px] font-light'>
+        <p className='z-[7] mt-[24px] md:mt-[40px] font-light mb-[8px] lg:text-[20px]'>
           ...where you can belong to a school club, a gaming group, or a
           worldwide art community. Where just you and a handful of friends can
           spend time together. A place that makes it easy to talk every day and
           hang out more often.
         </p>
-        <div className='flex md:flex-col flex-wrap'>
+        <div className='z-[7] flex md:flex-col flex-wrap justify-start lg:justify-center lg:flex-row'>
           <a
             href='/'
             className='flex items-center w-fit justify-center text-primary text-[18px] font-medium items-center space-x-2 mt-[24px] mr-[24px] px-[32px] py-[16px] bg-white rounded-full'
@@ -37,7 +37,25 @@ const Header = (props: Props) => {
         </div>
       </div>
       {/* Images */}
-      <div></div>
+      <div className='relative select-none md:absolute md:top-0 md:left-0 md:h-full md:w-full'>
+        <img
+          src='/img/hero1.svg'
+          alt='hero image'
+          className='relative z-20 block md:hidden lg:block w-full max-w-[880px] z-10 ml-[-80px] lg:left-[50%] lg:h-auto lg:w-auto lg:bottom-0 lg:ml-[-1030px] lg:absolute lg:transform-none lg:max-w-none'
+        />
+        <img
+          src='/img/hero2.svg'
+          alt='hero image'
+          className='absolute z-20 bottom-0 hidden md:block md:left-[50%] md:ml-[-70px] lg:ml-[370px]'
+        />
+        <img
+          src='/img/hero3.svg'
+          alt='hero image'
+          className='absolute bottom-0 z-[4] hidden top-0 md:inset-0 md:ml-[-1280px] md:top-auto md:bottom-0 md:block md:left-auto'
+          width='2560'
+          height='626'
+        />
+      </div>
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,36 +1,67 @@
-import React from 'react'
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {FaDiscord} from 'react-icons/fa'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaDiscord } from 'react-icons/fa';
+import Image from 'next/image';
+import Link from 'next/link';
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <nav className='grid grid-cols-2 lg:grid-cols-5 w-full px-[24px] min-h-[80px] items-center text-white'>
+    <nav className='grid grid-cols-2 lg:grid-cols-4 max-w-[1180px] mx-auto px-[24px] md:px-[40px] min-h-[80px] items-center text-white'>
       {/* Logo */}
-      <div className='flex space-x-2 items-center col-span-1'>
+      <div className='flex space-x-2 items-center col-span-1 w-fit'>
         <FaDiscord className='w-[40px] h-[40px] p-1' />
-        <Image alt='logo' src="/img/discord.png" width='100' height='10' className='py-3'/>
+        <Image
+          alt='logo'
+          src='/img/discord.png'
+          width='100'
+          height='10'
+          className='py-3'
+        />
       </div>
       {/* Nav Items */}
-      <div className='hidden lg:inline-grid lg:col-span-3 text-[1rem]'>
+      <div className='hidden lg:inline-grid lg:col-span-2 text-[1rem]'>
         <ul className='flex space-x-10 font-medium'>
           {/* Menu items */}
-          <li><Link href='/'>Download</Link></li>
-          <li><Link href='/'>Nitro</Link></li>
-          <li><Link href='/'>Discover</Link></li>
-          <li><Link href='/'>Safety</Link></li>
-          <li><Link href='/'>Support</Link></li>
-          <li><Link href='/'>Blog</Link></li>
-          <li><Link href='/'>Careers</Link></li>
+          <li>
+            <Link href='/'>Download</Link>
+          </li>
+          <li>
+            <Link href='/'>Nitro</Link>
+          </li>
+          <li>
+            <Link href='/'>Discover</Link>
+          </li>
+          <li>
+            <Link href='/'>Safety</Link>
+          </li>
+          <li>
+            <Link href='/'>Support</Link>
+          </li>
+          <li>
+            <Link href='/'>Blog</Link>
+          </li>
+          <li>
+            <Link href='/'>Careers</Link>
+          </li>
         </ul>
-        </div>
+      </div>
+      {/* Empty Span */}
+      <div className='hidden lg:inline-grid lg:col-span-1 justify-end'>
+        <a
+          href='/'
+          className='px-[1rem] py-[7px] rounded-full text-primary bg-white text-[14px] w-fit'
+        >
+          Login
+        </a>
+      </div>
       {/* Hamburger */}
-      <div className='inline-grid lg:hidden justify-end justify-end'><GiHamburgerMenu  className='w-[40px] h-[40px] p-1'/></div>
+      <div className='inline-grid lg:hidden justify-end justify-end'>
+        <GiHamburgerMenu className='w-[40px] h-[40px] p-1' />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
